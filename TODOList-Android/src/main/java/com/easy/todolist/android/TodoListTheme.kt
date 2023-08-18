c79c6abfc49d19e813d49d113e9f54fb67d28f0d
= <=>
2023-08-18 17:54:25 +0800

    update android theme

--- TODOList-Android/src/main/java/com/easy/todolist/android/TodoListTheme.kt
+++ TODOList-Android/src/main/java/com/easy/todolist/android/TodoListTheme.kt
@@ -73,11 +73,11 @@ fun TodoListTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(
                 window,
                 view
-            ).isAppearanceLightStatusBars = darkTheme
+            ).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
